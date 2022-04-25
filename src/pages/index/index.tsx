0099ea4866a8560a9cbab32d09c1e72fb41c5ed6
fanzehua <316679581@qq.com>
2022-04-25 13:07:36 +0800

    项目初次提交

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import { View, Text } from "@tarojs/components";
+import "./index.scss";
+
+export default (props: any) => {
+  return (
+    <View className="index">
+      <Text>Hello world!</Text>
+    </View>
+  );
+};
